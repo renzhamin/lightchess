9fc16cc27b274b4c5e6d8dbffb28e194107718a7
renzhamin <tasnimferdous9@iut-dhaka.edu>
2022-09-09 10:12:51 +0600

    GetUsers fixed

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const getUsers = async(req, res) => {
     try {
-        const users = await Users.findOne({
+        const users = await Users.findAll({
             attributes:['id','name','email']
         });
         res.json(users);
