8157614325a3575fdaca0d6e8c4d3105b2f9b91b
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2022-11-19 02:00:38 +0600

    dummy stats table created at profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -6,6 +6,17 @@ import { useEffect, useState } from "react"
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
 import { Pie } from "react-chartjs-2"
 import Chart from "react-apexcharts"
+import Box from "@mui/material/Box"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import { CardActionArea } from "@mui/material"
+import Typography from "@mui/material/Typography"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
@@ -69,7 +80,6 @@ function Profile(props) {
                 response.data.losses,
                 response.data.draws,
             ]
-            console.log(gameHistory)
 
             setPieChartData({
                 labels: ["Wins", "Losses", "Draws"],
@@ -78,11 +88,11 @@ function Profile(props) {
                         label: "Game History",
                         data: gameHistory,
                         backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
+                            "rgb(145, 184, 102)",
+                            "rgb(219, 113, 113)",
+                            "rgb(211, 227, 246)",
                         ],
-                        borderWidth: 1,
+                        borderWidth: 2,
                     },
                 ],
             })
@@ -98,15 +108,67 @@ function Profile(props) {
                     data: eloHistory,
                 },
             ])
-
-            console.log(eloHistory)
         }
 
         getUserInfo()
     }, [])
 
+    function getData(title, num) {
+        return { title, num }
+    }
+
+    const realRows = [
+        getData("Total games", 10),
+        getData("Won as white", 2),
+        getData("Won as black", 8),
+        getData("Games drawn", 0),
+    ]
+
     return (
         <Container component="main" alignItems="center">
+            <Typography align="center" variant="h3" gutterBottom>
+                {username}
+            </Typography>
+
+            <Paper
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                elevation={3}
+                align="center"
+                sx={{ width: 300 }}
+            >
+                <TableContainer>
+                    <Table
+                        sx={{ width: 300 }}
+                        size="small"
+                        aria-label="a dense table"
+                        align="center"
+                    >
+                        <TableBody>
+                            {realRows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{
+                                        "&:last-child td, &:last-child th": {
+                                            border: 0,
+                                        },
+                                    }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.title}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {row.num}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
             <Grid
                 container
                 spacing={0}
@@ -122,13 +184,29 @@ function Profile(props) {
                         options={{ maintainAspectRatio: false }}
                     />
                 </Grid>
+                <Typography gutterBottom variant="h5" component="div">
+                    Win/Loss stats
+                </Typography>
                 <Grid item mt={10}>
-                    <Chart
-                        options={lineChartOptions}
-                        series={lineChartSeries}
-                        type="line"
-                        width="500"
-                    />
+                    <Card sx={{ maxWidth: 720 }}>
+                        <Chart
+                            options={lineChartOptions}
+                            series={lineChartSeries}
+                            type="line"
+                            width="720"
+                        />
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="div"
+                                >
+                                    Elo Graph
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 </Grid>
             </Grid>
         </Container>
