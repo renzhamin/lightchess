1fe8f8993107035fff94000a3c52c699c1ebd1ac
ssshanto <ssshanto@gmail.com>
2022-11-21 08:57:40 +0600

    home button after game ends

--- frontend/src/components/GameEndDialog.js
+++ frontend/src/components/GameEndDialog.js
@@ -9,25 +9,32 @@ import ListItemText from "@mui/material/ListItemText"
 import DialogTitle from "@mui/material/DialogTitle"
 import Dialog from "@mui/material/Dialog"
 import { DialogContent, DialogContentText } from "@mui/material"
+import { useHistory } from "react-router-dom"
 
 function GameEndDialog(props) {
     const { onClose, open } = props
     var message = ""
+    const history = useHistory()
 
     const handleClose = () => {
         onClose()
     }
 
+    const Home = () => {
+        history.push("/")
+    }
+
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle align="center">
-                {props.gameEndTitle}
-            </DialogTitle>
+            <DialogTitle align="center">{props.gameEndTitle}</DialogTitle>
             <DialogContent>
                 <DialogContentText id="game-description" align="center">
                     {props.gameEndMessage}
                 </DialogContentText>
             </DialogContent>
+            <Button variant="contained" onClick={Home}>
+                HOME
+            </Button>
         </Dialog>
     )
 }
