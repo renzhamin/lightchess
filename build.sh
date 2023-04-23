ddeda66262c0ca7a0e0656b9d1d265566eea2664
renzhamin <renzhamin@gmail.com>
2023-04-24 01:37:19 +0600

    run all commands sequentially in the build script

--- build.sh
+++ build.sh
@@ -8,12 +8,21 @@ npm install yarn
 
 yarn="./node_modules/yarn/bin/yarn"
 
+
+
 echo ".........Installing frontend packages........."
 ${yarn} --cwd="./frontend"
 
 echo ".........Installing backend packages........."
 ${yarn} --cwd="./backend"
 
-echo "..........Building app............"
-${yarn} --cwd="./backend" build &
-${yarn} --cwd="./frontend" build && mv ./frontend/build backend/dist
+
+
+
+echo "..........Building frontend............"
+${yarn} --cwd="./frontend" run build >& /dev/null
+
+echo "..........Building backend............"
+rm -r backend/dist
+${yarn} --cwd="./backend" run build
+mv ./frontend/build backend/dist
