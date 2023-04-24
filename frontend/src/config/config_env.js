cfaf9cd07e57f5d3b1db105bc29f36367a73c36a
renzhamin <renzhamin@gmail.com>
2023-04-24 13:45:15 +0600

    provide backend url in dev env in code

--- frontend/src/config/config_env.js
+++ frontend/src/config/config_env.js
@@ -2,8 +2,8 @@ let backend = ""
 let backend_ws = ""
 
 if (process.env.NODE_ENV === "development") {
-    backend = process.env.REACT_APP_BACKEND_URL
-    backend_ws = process.env.REACT_APP_WEB_SOCKET_URL
+    backend = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
+    backend_ws = process.env.REACT_APP_WEB_SOCKET_URL || "ws://localhost:5000"
 }
 
 export const config = {
