e6fc7fc564235277b8a4fc96d48305266b199c8d
renzhamin <renzhamin@gmail.com>
2023-09-03 22:03:37 +0600

    empty backend_url string for production

--- frontend/src/config/config_env.js
+++ frontend/src/config/config_env.js
@@ -1,4 +1,7 @@
-const backend = process.env.REACT_APP_BACKEND_URL || ""
+let backend = ""
+
+if (process.env.NODE_ENV === "development")
+    backend = process.env.REACT_APP_BACKEND_URL
 
 export const config = {
     backend,
